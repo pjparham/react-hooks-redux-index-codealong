85e6538cc3f61d428a6c7e7dbd3909824f9d6c63
Parker Parham <parkerparham@gmail.com>
2023-02-17 12:52:01 -0500

    Done with assignmnent

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Todo from "./Todo";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  const todos = useSelector((state) => state.todos.entities);
+
+  const todoList = todos.map((todo, index) => <Todo key={index} text={todo}/>)
+
+  console.log(todos)
+  return <div>{todoList}</div>;
 }
 
 export default TodoContainer;
